68bf064e062c53f6dd8e299fae0e4f3ae09ad8ad
khairil.yusof@gmail.com <khairi.yusof@gmail.com>
2013-06-28 18:42:20 +0800

    Add initial service fields.

---
+++ sinarngo/service/content/service.py
@@ -0,0 +1,46 @@
+from five import grok
+from plone.directives import dexterity, form
+
+from zope import schema
+from zope.schema.interfaces import IContextSourceBinder
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+
+from zope.interface import invariant, Invalid
+
+from z3c.form import group, field
+
+from plone.namedfile.interfaces import IImageScaleTraversable
+from plone.namedfile.field import NamedImage, NamedFile
+from plone.namedfile.field import NamedBlobImage, NamedBlobFile
+
+from plone.app.textfield import RichText
+
+from z3c.relationfield.schema import RelationList, RelationChoice
+from plone.formwidget.contenttree import ObjPathSourceBinder
+from plone.multilingualbehavior.directives import languageindependent
+
+from sinarngo.service import MessageFactory as _
+
+
+# Interface class; used to define content-type schema.
+
+class IService(form.Schema, IImageScaleTraversable):
+    """
+    Online Community and Services Content Type for Plone
+    """
+
+    title = schema.TextLine(title=u'Name', 
+                         description=u'Name of service.')
+
+    description = schema.TextLine(title=u'Description',
+                                  description=u'Brief description '
+                                  'of service.'
+                                  )
+
+    url = schema.TextLine(title=u'Link to service',
+                          description=u'eg. http://sinarproject.org')
+
+    details = RichText(
+                title=_(u"Details"),
+                required=False,
+            ) 
